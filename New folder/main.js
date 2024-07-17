906076c99be669613cac608dcf046ea836d57372
roxana99999 <roserose0955@gmail.com>
2024-07-17 18:51:59 -0400

    first commit

---
+++ New folder/main.js
@@ -0,0 +1,13 @@
+document.querySelector("h1").style.color = "red";
+document.write("<p>test</p>");
+document.write("<p>test</p>");
+document.write("<p>test</p>");
+document.write("<p>test</p>");
+document.write("<p>test</p>");
+var x;
+document.write(typeof x);
+let name = "roxana";
+let age = 24;
+console.log("my name is" +" " + name + " " + "my age is"+" " + age);
+ console.log(`my name is ${name} and my age is ${age}`);
+ 
